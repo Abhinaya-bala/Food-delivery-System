a7c8ab6200fad822583bffb1d1b1fbff6e8b0a39
Abhinaya-bala <abhinayafeb19@gmail.com>
2020-06-12 19:57:06 +0530

    created Required Components and data

---
+++ src/Components/OrderDetails.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+import './App.css';
+
+class OrderDetails extends Component {
+    constructor(props) {
+        super(props)
+
+
+    }
+
+
+
+    render() {
+        return (
+            <div >
+
+
+
+            </div>
+
+        )
+    }
+
+}
+
+export default OrderDetails;
---
+++ src/Components/SalesReport.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+import './App.css';
+
+class SalesReport extends Component {
+    constructor(props) {
+        super(props)
+
+
+    }
+
+
+
+    render() {
+        return (
+            <div >
+
+
+
+            </div>
+
+        )
+    }
+
+}
+
+export default SalesReport;
